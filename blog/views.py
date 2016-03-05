b02030dac624161bb6c5e5c5981a6cc5e7fe26dc
carolina <gcyr189@hotmail.com>
2016-03-05 15:52:43 -0500

    Blog con tablas

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Fotos
 
 # Create your views here.
 def post_list(request):
-        return render(request, 'blog/post_list.html', {})
+    posts = Fotos.objects.order_by('fecha')
+    return render(request, 'blog/post_list.html', {'fotos':posts})
